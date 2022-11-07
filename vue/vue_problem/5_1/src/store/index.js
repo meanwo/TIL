80db7d95e4af7e99daf46d38788f5a1c3e67eae3
meanwo <meanwo0603@likelion.org>
2022-11-07 17:58:00 +0900

    11/07 #1

---
+++ vue/vue_problem/5_1/src/store/index.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    orderList: [],
+    menuList: [
+      {
+        title: '아메리카노',
+        price: 3000,
+        selected: true,
+        image: '<https://source.unsplash.com/featured/?americano>'
+      },
+    ],
+    sizeList: [
+      {
+        name:'small',
+        price: 500,
+      }
+    ],
+  },
+  getters: {
+  },
+  mutations: {
+    addOrder: function () {},
+    updateMenuList: function () {},
+    updateSizeList: function () {},
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
